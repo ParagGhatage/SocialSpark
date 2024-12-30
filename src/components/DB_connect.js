e1bbeac3bb5d6d7509523d1278088b246d4e29bb
ParagGhatage <phghatage1@gmail.com>
2024-12-30 14:29:35 +0530

    some minor frontend changes made.

--- src/components/DB_connect.js
+++ src/components/DB_connect.js
@@ -3,7 +3,7 @@ import { DataAPIClient } from "@datastax/astra-db-ts";
 export default async function DBconnect() {
   try {
     // Initialize the client with your AstraDB token
-    const client = new DataAPIClient('AstraCS:zAqPuWBioSggQqNJqhwgPTQH:02b61e5a6f6fc2cd2189d6c29d66a311dc1528ee98d9e3ccb71e4c2c83d1f37a');
+    const client = new DataAPIClient(process.env.ASTRA_TOKEN);
 
     // Connect to your Astra DB instance
     const db = client.db('https://3cf7c373-0cfb-4bf3-80f9-8d2ffdcf65ae-us-east-2.apps.astra.datastax.com');
